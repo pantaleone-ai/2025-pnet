8d74216fd581bd6b775254ef8ee793691ccbb549
pantaleone-ai <mdptrading@gmail.com>
2024-12-26 16:11:21 -0600

    update

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const withNextra = require("nextra")({
  
 module.exports = withNextra({
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
